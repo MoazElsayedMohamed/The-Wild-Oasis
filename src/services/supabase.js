02828340e234a7a9df57463019dc12a8695703a8
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-09 02:10:43 +0300

    feat: :wrench: create supabase client service

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hpypmyvrijocdsutkpvx.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhweXBteXZyaWpvY2RzdXRrcHZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc4ODQzMDUsImV4cCI6MjAzMzQ2MDMwNX0.uEGkXq89JhW1EtzywLd11QL3iesMwAYWiZcPyBjmjpk";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
