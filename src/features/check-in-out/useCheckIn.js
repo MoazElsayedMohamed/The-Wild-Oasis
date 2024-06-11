9108907f4dff436cf682453f1c6d7d99caeae450
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-12 02:08:14 +0300

    feat: :sparkles: run check in button and change the status state

---
+++ src/features/check-in-out/useCheckIn.js
@@ -0,0 +1,27 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function CheckIn() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutate: checkin, isLoading: isCheckingIn } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "check-in",
+        isPaid: true,
+      }),
+    onSuccess: (data) => {
+      toast.success(`booking #${data.id} successfully checked in`);
+      queryClient.invalidateQueries({ active: true });
+      navigate("/");
+    },
+
+    onError: () => toast.error("There was an error while checking in"),
+  });
+
+  return { checkin, isCheckingIn };
+}
+
+export default CheckIn;
