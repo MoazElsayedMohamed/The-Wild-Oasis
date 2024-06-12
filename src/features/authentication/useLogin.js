0c7da4dd76d8b9289eb9a85787282d1efcb32863
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-06-12 19:15:36 +0300

    feat: :sparkles: create login page and authenticate login user

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,18 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { login as loginApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate("/dashboard");
+    },
+    onError: (err) => toast.error("provided email or password is incorrect"),
+  });
+
+  return { login, isLoading };
+}
